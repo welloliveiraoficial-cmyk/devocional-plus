6a9d9558e57c631e6bdd420e023204d70a5d783b
welloliveiraoficial-cmyk <welloliveiraoficial@gmail.com>
2026-08-01 17:33:30 -0300

    Update home_header.dart

--- lib/widgets/home/home_header.dart
+++ lib/widgets/home/home_header.dart
@@ -15,66 +15,73 @@ class HomeHeader extends StatefulWidget {
 
 class _HomeHeaderState extends State<HomeHeader>
     with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
+  late AnimationController _entryController;
+  late Animation<double> _entryAnimation;
 
-  late AnimationController _sunController;
-  late Animation<double> _sunAnimation;
+  late AnimationController _symbolController;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _floatAnimation;
+
+  bool get isDay {
+    final hour = DateTime.now().hour;
+    return hour >= 6 && hour < 18;
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
+    _entryController = AnimationController(
       duration: const Duration(milliseconds: 900),
       vsync: this,
     );
 
-    _animation = CurvedAnimation(
-      parent: _controller,
+    _entryAnimation = CurvedAnimation(
+      parent: _entryController,
       curve: Curves.easeOut,
     );
 
-    _controller.forward();
+    _entryController.forward();
 
-
-    _sunController = AnimationController(
-      duration: const Duration(seconds: 3),
+    _symbolController = AnimationController(
+      duration: const Duration(seconds: 5),
       vsync: this,
     );
 
-    _sunAnimation = Tween<double>(
-      begin: -4,
-      end: 4,
+    _rotationAnimation = Tween<double>(
+      begin: 0,
+      end: 6.28,
+    ).animate(_symbolController);
+
+    _floatAnimation = Tween<double>(
+      begin: -5,
+      end: 5,
     ).animate(
       CurvedAnimation(
-        parent: _sunController,
+        parent: _symbolController,
         curve: Curves.easeInOut,
       ),
     );
 
-    _sunController.repeat(
-      reverse: true,
-    );
+    _symbolController.repeat();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
-    _sunController.dispose();
+    _entryController.dispose();
+    _symbolController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return FadeTransition(
-      opacity: _animation,
+      opacity: _entryAnimation,
       child: SlideTransition(
         position: Tween<Offset>(
           begin: const Offset(0, -0.15),
           end: Offset.zero,
-        ).animate(_animation),
-
+        ).animate(_entryAnimation),
         child: Container(
           padding: const EdgeInsets.fromLTRB(
             22,
@@ -82,7 +89,6 @@ class _HomeHeaderState extends State<HomeHeader>
             22,
             30,
           ),
-
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
@@ -92,17 +98,13 @@ class _HomeHeaderState extends State<HomeHeader>
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
-
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(35),
               bottomRight: Radius.circular(35),
             ),
           ),
-
           child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start,
-
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
 
               Text(
@@ -129,62 +131,63 @@ class _HomeHeaderState extends State<HomeHeader>
 
               const SizedBox(height: 20),
 
-
               Container(
                 padding: const EdgeInsets.all(18),
-
                 decoration: BoxDecoration(
                   color: Colors.white.withOpacity(0.08),
-                  borderRadius:
-                      BorderRadius.circular(22),
-
+                  borderRadius: BorderRadius.circular(22),
                   border: Border.all(
                     color: AppColors.bronze,
                     width: 0.5,
                   ),
                 ),
-
                 child: Row(
                   children: [
 
                     AnimatedBuilder(
-                      animation: _sunAnimation,
-
+                      animation: _symbolController,
                       builder: (context, child) {
+
                         return Transform.translate(
                           offset: Offset(
                             0,
-                            _sunAnimation.value,
+                            isDay
+                                ? 0
+                                : _floatAnimation.value,
                           ),
 
-                          child: Container(
-                            width: 48,
-                            height: 48,
+                          child: Transform.rotate(
+                            angle: isDay
+                                ? _rotationAnimation.value
+                                : 0,
 
-                            decoration: BoxDecoration(
-                              color: AppColors.bronze
-                                  .withOpacity(0.18),
-                              shape: BoxShape.circle,
-                            ),
+                            child: Container(
+                              width: 48,
+                              height: 48,
+                              decoration: BoxDecoration(
+                                color: AppColors.bronze
+                                    .withOpacity(0.18),
+                                shape: BoxShape.circle,
+                              ),
 
-                            child: const Icon(
-                              Icons.wb_sunny_rounded,
-                              color: AppColors.bronze,
-                              size: 26,
+                              child: Icon(
+                                isDay
+                                    ? Icons.wb_sunny_rounded
+                                    : Icons.nightlight_round,
+                                color: AppColors.bronze,
+                                size: 26,
+                              ),
                             ),
                           ),
                         );
                       },
                     ),
 
-
                     const SizedBox(width: 14),
-
 
                     const Expanded(
                       child: Text(
                         'Comece seu dia com fé, oração e uma palavra de esperança.',
-
                         style: TextStyle(
                           color: Colors.white70,
                           fontSize: 14,
